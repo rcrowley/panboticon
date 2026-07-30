db84b6f694b4186aec3cc3f239938fb54a975eeb
Richard Crowley <rcrowley@planetscale.com>
2026-07-29 17:41:10 -0700

    GitHub App instead of GitHub user for the bot

--- lib/panboticon/configure.sh
+++ lib/panboticon/configure.sh
@@ -23,6 +23,9 @@ export PANBOTICON_CONFIG
         PANBOTICON_BOT_USER \
         PANBOTICON_HUMAN_EMAIL \
         PANBOTICON_BOT_EMAIL \
+        PANBOTICON_BOT_GITHUB_APP_ID \
+        PANBOTICON_BOT_GITHUB_APP_INSTALLATION_ID \
+        PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY \
         PANBOTICON_HUMAN_SHELL \
         PANBOTICON_BOT_SHELL \
         PANBOTICON_HUMAN_BOOTSTRAP \
@@ -39,7 +42,7 @@ export PANBOTICON_CONFIG
 } 3>"$TMP/filter.jq" 4>"$TMP/export.sh"
 
 jq -e -f "$TMP/filter.jq" -r <"$PANBOTICON_CONFIG" |
-grep -E "^: \\\$\\{PANBOTICON_[_A-Z]+:=\"[-./_~@0-9A-Za-z]*\"\\}\$" >"$TMP/config.sh"
+grep -E "^: \\\$\\{PANBOTICON_[_0-9A-Z]+:=\"[-./_~@0-9:A-Za-z]*\"\\}\$" >"$TMP/config.sh"
 
 . "$TMP/config.sh"
 . "$TMP/export.sh"
@@ -60,6 +63,13 @@ case "$PANBOTICON_HUMAN_BOOTSTRAP" in
     "~/"*) PANBOTICON_HUMAN_BOOTSTRAP="$HOME/${PANBOTICON_HUMAN_BOOTSTRAP#"~/"}";;
     "/"*) ;;
     *) PANBOTICON_HUMAN_BOOTSTRAP="$(dirname "$PANBOTICON_CONFIG")/$PANBOTICON_HUMAN_BOOTSTRAP";;
+esac
+case "$PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY" in
+    ""|"op://"*) ;;
+    "~") PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY="";;
+    "~/"*) PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY="$HOME/${PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY#"~/"}";;
+    "/"*) ;;
+    *) PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY="$(dirname "$PANBOTICON_CONFIG")/$PANBOTICON_BOT_GITHUB_APP_PRIVATE_KEY";;
 esac
 case "$PANBOTICON_BOT_BOOTSTRAP" in
     "") ;;
